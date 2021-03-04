e3c6e09992cfe7aecd53c341e43f8f884730a28d
sutanervin-dev <sutanervinlorenza@gmail.com>
2021-03-04 12:39:03 +0700

    solve bearer token

--- components/RetailCard.js
+++ components/RetailCard.js
@@ -1,21 +1,29 @@
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
-
-
 function ProductCard (props) {
   const data = props.data
 
   return (
-    <div className="col-lg-3 mt-3">
+      <>
+      {
+         data.category === props.category ? (
+             <>
+            <div className="col-lg-3 mt-3">
       <div className="card border-light shadow h-100" style={{ width: 'auto' }}>
         <div className="card-body">
           <img src={ data.product_image } className="card-img-top" alt={ data.title } style={{ width: '100%', height: '20vh', objectFit: 'contain' }} />
           <h5 className="card-title text-center">{ data.title }</h5>
+          <h5 className="card-title text-center">{ data.category }</h5>
+
         </div>
           <p className="card-text text-center" style={{ color: 'green'}}>Rp. { data.price }</p>
       </div>
     </div>
+    </>
+         ) : (
+            <>
+            </>
+         )
+      }
+    </>
   )
 }
 
--- pages/Home.js
+++ pages/Home.js
@@ -1,21 +1,36 @@
 import React, { useEffect } from 'react'
 import ProductCard from '../components/ProductCard'
+import RetailCard from '../components/RetailCard'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchFlashSale } from '../store/actions/productAction'
+import { fetchFlashSale, fetchRetail } from '../store/actions/productAction'
 
 function Home () {
-  const { flashSaleProducts, isLoading } = useSelector(state => state.products)
+  const { flashSaleProducts, retailProduct, isLoading } = useSelector(state => state.products)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(fetchFlashSale())
+    dispatch(fetchRetail())
   }, [])
 
   return (
     <>
     {
-      JSON.stringify(flashSaleProducts)
-    }
+        isLoading ? (
+          <div className="spinner-border m-5" role="status"></div>
+        ) : (
+          <div className="container">
+            <h1>Flash Sale</h1>
+            <div className="row">
+              {
+                flashSaleProducts.data.map(product => {
+                  return <ProductCard key={ product.product_id } data={ product } />
+                })
+              }
+            </div>
+          </div>
+        )
+      }
     </>
   )
 }
