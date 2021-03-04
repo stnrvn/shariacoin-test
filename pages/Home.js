0a5ccaec43ddd919af3d84150277157ea629edb0
sutanervin-dev <sutanervinlorenza@gmail.com>
2021-03-04 12:45:24 +0700

    adding product by category

--- pages/Home.js
+++ pages/Home.js
@@ -31,6 +31,40 @@ function Home () {
           </div>
         )
       }
+
+      {
+        isLoading ? (
+          <div className="spinner-border m-5" role="status"></div>
+        ) : (
+          <div className="container mt-5">
+            <h1>Antam</h1>
+            <div className="row">
+              {
+                retailProduct.data.map(product => {
+                  return <RetailCard key={ product.product_id } data={ product } category='Antam' />
+                })
+              }
+            </div>
+          </div>
+        )
+      }
+
+      {
+        isLoading ? (
+          <div className="spinner-border m-5" role="status"></div>
+        ) : (
+          <div className="container mt-5">
+            <h1>Big Gold</h1>
+            <div className="row">
+              {
+                retailProduct.data.map(product => {
+                  return <RetailCard key={ product.product_id } data={ product } category='BIG Gold' />
+                })
+              }
+            </div>
+          </div>
+        )
+      }
     </>
   )
 }
